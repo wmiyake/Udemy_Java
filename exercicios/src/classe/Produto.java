40e7f4b016f28ed0dbf5ab116041afca6affffe0
Wellington Miyake <wellington.miyake@ibm.com>
2021-07-23 04:20:50 -0300

    CLASSES & METODOS

---
+++ exercicios/src/classe/Produto.java
@@ -0,0 +1,23 @@
+package classe;
+
+public class Produto {
+
+	String nome;
+	double preco;
+	static double desconto = 0.25;
+	
+	Produto(){
+	}
+	
+	Produto(String nomeInicial, double precoInicial){
+		nome = nomeInicial;
+		preco = precoInicial;
+	}
+	
+	double precoComDesconto() {
+		return preco * (1 - desconto);
+	}
+	double precoComDesconto(double descontDoGerente) {
+		return preco * (1 - desconto + descontDoGerente);
+	}
+}
