60e4305e6acf8ea723e9ee0f2bb80a1855f16d1f
Wellington Miyake <wellington.miyake@ibm.com>
2021-08-12 00:32:54 -0300

    Projeto Campo Minado parte 2

--- campo-minado/src/br/com/cod3r/com/modelo/Campo.java
+++ campo-minado/src/br/com/cod3r/com/modelo/Campo.java
@@ -3,6 +3,8 @@ package br.com.cod3r.com.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.cod3r.com.excecao.ExplosaoException;
+
 public class Campo {
 
 	private final int linha;
@@ -14,12 +16,72 @@ public class Campo {
 	
 	private List<Campo> vizinhos = new ArrayList<>();
 	
-	Campo(int linha, int coluna){
+	public Campo(int linha, int coluna){
 		this.linha = linha;
 		this.coluna = coluna;
 	}
 	
-	boolean adicionarVizinho(Campo vizinho) {
+	public boolean adicionarVizinho(Campo vizinho) {
+		boolean linhaDiferente = linha != vizinho.linha;
+		boolean colunaDiferente = coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
 		
+		int deltaLinha = Math.abs(linha - vizinho.linha);
+		int deltaColuna = Math.abs(coluna - vizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+		
+		if(deltaGeral == 1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		}else if(deltaGeral == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void alternarMarcacao() {
+		if(!aberto) {
+			marcado = !marcado;
+		}
+	}
+	
+	public boolean abrir() {
+		if(!aberto && !marcado) {
+			aberto = true;
+			
+			if(minado) {
+				throw new ExplosaoException();
+			}
+			if(vizinhancaSegura()) {
+				vizinhos.forEach(v -> v.abrir());
+			}
+			return true;
+		}else {
+			
+			return false ;
+		}
+		
+		
+	}
+	
+	boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
+	}
+	
+	public void minar() {
+		minado = true;
+	}
+	
+	public boolean isMarcado() {
+		return marcado;
+	}
+	
+	public boolean isAberto() {
+		return aberto;
+	}
+	public boolean isFechado() {
+		return !isAberto();
 	}
 }
