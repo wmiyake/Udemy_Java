bb6b74b78f3cb94b2e982c80b7c72771dc93ff7f
Wellington Miyake <wellington.miyake@ibm.com>
2021-08-15 21:30:53 -0300

    JAVA CONEXAO FORA DO ESCOPO

--- exercicios/src/jdbc/CriarBanco.java
+++ exercicios/src/jdbc/CriarBanco.java
@@ -1,18 +1,19 @@
 package jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class CriarBanco {
 
 	public static void main(String[] args) throws SQLException {
-		final String url = "jdbc:mysql://localhost:3306";
-		final String usuario = "root";
-		final String senha = "wm36ctco";
+//		final String url = "jdbc:mysql://localhost:3306";
+//		final String usuario = "root";
+//		final String senha = "wm36ctco";
+//		
+//		Connection conexao = DriverManager.getConnection(url, usuario, senha);
 		
-		Connection conexao = DriverManager.getConnection(url, usuario, senha);
+		Connection conexao = FabricaConexao.getConexao();
 		
 		Statement stmt = conexao.createStatement();
 		stmt.execute("CREATE DATABASE IF NOT EXISTS curso_java_2");
---
+++ exercicios/src/jdbc/FabricaConexao.java
@@ -0,0 +1,20 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+
+	public static Connection getConexao() {
+		try {
+			final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
+			final String usuario = "root";
+			final String senha = "wm36ctco";
+			
+			return DriverManager.getConnection(url, usuario, senha);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
