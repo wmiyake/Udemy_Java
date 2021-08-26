fd97fb5a9b3653dffac76f071fed6c3179b8c6f9
Wellington Miyake <wellington.miyake@ibm.com>
2021-08-26 03:11:33 -0300

    Consulta Paginadas

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/conttrollers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/conttrollers/ProdutoController.java
@@ -1,11 +1,18 @@
 package br.com.cod3r.exerciciossb.conttrollers;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,15 +26,46 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Produto salvarPorduto(@Valid Produto produto) {
+		produtoRepository.save(produto);
+		return produto;
+	}
+	
 	@PostMapping
 	public @ResponseBody Produto novoPorduto(@Valid Produto produto) {
 		//System.out.println(produto.repository);
 		produtoRepository.save(produto);
 		return produto;
-	}
+		}
+
 	//Retorna todos produtos
 	@GetMapping
 	public Iterable<Produto> obterProdutos() {
 		return produtoRepository.findAll();
+	}
+	
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
+	public Iterable<Produto> obterProdutosPorPagina(
+			@PathVariable  int numeroPagina, @PathVariable  int qtdePagina){
+		if(qtdePagina >= 5) qtdePagina = 5;
+		Pageable page = PageRequest.of(numeroPagina, qtdePagina);
+		return produtoRepository.findAll(page);
+	}
+	
+	@GetMapping(path = "{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
+	}
+	
+//	@PutMapping
+//	public Produto alterarProduto(Produto produto) {
+//		produtoRepository.save(produto);
+//		return produto;
+//	}
+	
+	@DeleteMapping(path = "/{id}")
+	public void excluirProduto(@PathVariable int id ) {
+		produtoRepository.deleteById(id);
 	}
 }
--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
@@ -1,10 +1,10 @@
 package br.com.cod3r.exerciciossb.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.cod3r.exerciciossb.entities.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
 
 	
 }
